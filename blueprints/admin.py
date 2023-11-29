4645278a67072ba829406eca5d7e2832ffa72a73
R&z3r <86901031+CS-Zucker@users.noreply.github.com>
2023-11-29 20:51:03 +0800

    更新管理员登录功能、音乐管理功能

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,9 +1,43 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, session,g
 
+from .forms import *
+from exts import db
+from models import *
 
 
 # 管理员首页
 bp = Blueprint("admin", __name__, url_prefix="/admin")
 @bp.route('/')
+def admin_home():
+    return redirect(url_for("admin.admin_index"))
+
+@bp.route("/index")  # 就是把session信息全部清除
 def admin_index():
-    return render_template('admin_index.html')
+    return render_template("admin_index.html")
+
+#  一些授权操作，如登录和注册
+@bp.route("/login", methods=['GET','POST'])
+def admin_login():
+    if request.method == 'GET':
+        return render_template("admin_login.html")
+    else:
+        form = AdminForm(request.form)  # 记得把前端form的method改成post
+        if form.validate():
+            adminID = form.AdminId.data
+            password = form.Password.data
+            admin = Admin.query.filter_by(AdminID=adminID).first()  # 查找AdminID
+            if not admin:  # 管理员不存在
+                print("管理员不存在")
+                return redirect(url_for("admin.admin_login"))
+            else:
+                if admin.Password == password:
+                    # cookie用来存放登录授权的信息,cookie在浏览器里存储，这样就知道登录的用户是谁了
+                    session['admin_id'] = admin.AdminID
+                    return redirect(url_for("admin.admin_index"))
+                else:
+                    print("密码错误")
+                    return redirect(url_for("admin.admin_login"))
+        else:
+            print(form.errors)
+            return redirect(url_for("admin.admin_login"))  # 登录失败要重新渲染
+
--- blueprints/operate.py
+++ blueprints/operate.py
@@ -1,9 +1,9 @@
+import os
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for, flash, session
 from sqlalchemy import or_
 from exts import db
-from models import CommentModel, UserModel, OrderModel, OrderingModel
-from models import get_songs, get_songs_
-from .forms import AddUserForm, EditUserForm
+from models import *
+from .forms import *
 from decorators import login_required
 bp = Blueprint("operate", __name__, url_prefix="/operate")
 # 定义用一些操作
@@ -17,7 +17,7 @@ def search():
         musicname = request.form.get("m")  # 要搜索的歌曲
         if musicname is None:
             flash('请输入歌曲或歌手名称')
-            return render_template("search.html")  # 当没有提供歌曲名时给用户的提示
+            return render_template("search.html")  # 当没有提供歌曲名时给音乐的提示
         song_singers = get_songs(musicname)
         singer = musicname
         singer_songs = get_songs_(singer)
@@ -142,4 +142,112 @@ def user_edit(user_id):
             return redirect(url_for("operate.user_edit", user_id=user_id))
 
 
+# ===========================
 
+# 显示所有音乐的信息
+@bp.route('/music_list',methods=['GET','POST'])
+def music_list():
+    if request.method == 'GET':
+        page = request.args.get("page", type=int, default=1)   # 当没有参数时默认为一,转成整形很重要，默认为string
+        # 分页器对象
+        paginate = MusicModel.query.paginate(page=page, per_page=12)  # 当前页和每页展示多少数据
+
+        return render_template("music-list.html", paginate=paginate,)  # 把paginate对象传到前端
+    else:
+        music_id = request.form.get("musicid")  # 要搜索音乐id
+        if music_id == '':
+            flash("请输入数据")
+            return redirect(url_for("operate.music_list"))  # 当没有
+        else:
+            musics = MusicModel.query.filter(or_(MusicModel.MusicID == music_id, MusicModel.MusicName == music_id))
+            if not musics:
+                flash("不存在此音乐")
+                redirect(url_for("operate.music_list"))
+            else:
+                return redirect(url_for("operate.music_search", music_id=music_id))
+
+
+# 删除指定音乐的信息
+@bp.route('/music_delete/<string:music_id>')
+def music_delete(music_id):
+    drop_music = MusicModel.query.get(music_id)  # 删除音乐
+    db.session.delete(drop_music)
+    db.session.commit()
+    return redirect(url_for("operate.music_list"))
+
+# 搜索音乐信息
+@bp.route('/music_search/<string:music_id>',methods=['GET','POST'])
+def music_search(music_id):
+    if request.method == 'GET':
+        musicid = music_id
+        musicname = music_id
+        musics = MusicModel.query.filter(or_(MusicModel.MusicID.contains(musicid), MusicModel.MusicName.contains(musicname)))
+        return render_template("music-list.html", musics=musics, paginate=None)
+    else:
+        music_id = request.form.get("musicid")  # 要搜索音乐id
+        if music_id == '':
+            flash("请输入数据")
+            return redirect(url_for("operate.music_list"))  # 当没有
+        else:
+            return redirect(url_for("operate.music_search", music_id=music_id))
+# 添加音乐
+@bp.route('/music_add',methods=['GET','POST'])
+def music_add():
+    if request.method == 'GET':
+        return render_template('music-add.html')
+    else:
+        form = AddMusicForm(request.form)  # 获取前端音乐提交的form数据扔给registerform进行验证
+        if form.validate():  # 调用验证器和验证函数
+            musicf = request.files['musicfile']
+            coverf = request.files['coverfile']
+            musicf.save(os.path.join("static/musics/", musicf.filename))
+            coverf.save(os.path.join("static/covers/", coverf.filename))
+
+            musicid = form.MusicID.data
+            musicname = form.MusicName.data
+            intro = form.Intro.data
+            classid = form.ClassID.data
+            price = form.price.data
+            issuetime = form.IssueTime.data
+            storagelocation = os.path.join("../static/musics/", musicf.filename)
+            coverlocation = os.path.join("../static/covers/", coverf.filename)
+            music = MusicModel(MusicID=musicid, MusicName=musicname, ClassID=classid, Intro=intro, price=price, IssueTime=issuetime, StorageLocation=storagelocation, Cover=coverlocation)
+            db.session.add(music)
+            db.session.commit()
+            flash("音乐添加成功！")  # 添加Flash消息
+            return redirect(url_for("operate.music_list"))  # 把视图函数转换成url 蓝图.视图函数
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    flash(f'错误：{error} ')
+            return redirect(url_for("operate.music_add"))
+
+@bp.route('/music_edit/<string:music_id>',methods=['GET','POST'])
+def music_edit(music_id):
+    if request.method == 'GET':
+        return render_template('music-edit.html')
+    else:
+        form = EditMusicForm(request.form)  # 获取前端音乐提交的form数据扔给registerform进行验证
+        if form.validate():  # 调用验证器和验证函数
+            musicf = request.files['musicfile']
+            coverf = request.files['coverfile']
+            musicf.save(os.path.join("static/musics/", musicf.filename))
+            coverf.save(os.path.join("static/covers/", coverf.filename))
+
+            music = MusicModel.query.get(music_id)
+            music.MusicName = form.MusicName.data
+            music.Intro = form.Intro.data
+            music.ClassID = form.ClassID.data
+            music.price = form.price.data
+            music.IssueTime = form.IssueTime.data
+            music.StorageLocation = os.path.join("../static/musics/", musicf.filename)
+            music.Cover = os.path.join("../static/covers/", coverf.filename)
+            db.session.commit()
+            flash("音乐修改成功！")  # 添加Flash消息
+            return redirect(url_for("operate.music_list"))  # 把视图函数转换成url 蓝图.视图函数
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    flash(f'错误：{error} ')
+            print(form.errors)
+            return redirect(url_for("operate.music_edit", music_id=music_id))
